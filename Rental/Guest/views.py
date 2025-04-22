e4fabd2c4e1d2aa4377998f1352a69d481bd8027
Dheeraj Pandey <dheeraj.959595@gmail.com>
2025-04-22 18:27:10 +0530

    removed check line

--- Rental/Guest/views.py
+++ Rental/Guest/views.py
@@ -258,7 +258,6 @@ from user.models import Cloth
 from user.models import User
 
 
-#new implimentation
 @login_required(login_url='/login')
 def post_cloth(request):
     if request.method == "GET":
